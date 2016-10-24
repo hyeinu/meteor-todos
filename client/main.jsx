a5d22c06b7c8f69d689f574c2aae37ddf3cbdd12
Hyein <hyeinu65@gmail.com>
2016-10-24 19:38:52 -0400

    started adding the initial components

---
+++ client/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
+
+import App from './components/App.jsx';
+
+Meteor.startup(() => {
+  render(
+    <App />,
+    document.getElementById('root')
+  );
+})
